ad50b717bcdd39bccb6061575794500ffa6dc6fb
NiccoGrillo <niccolo.grillo@mail.polimi.it>
2024-06-01 08:04:38 +0200

    Implemented first methods: prints, split of f matrix (to be changed) and the JacobiIteration method. The comunications works properly, the calculations are not right yet

---
+++ src/JacobiSolver.cpp
@@ -0,0 +1,216 @@
+#include "JacobiSolver.hpp"
+#include <iostream>
+
+JacobiSolver::JacobiSolver(int rows, int cols, std::function<double(double, double)> func)
+    : n(rows), m(cols) {
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    initializeMatrix(func);
+
+    //now we can initialiaze the local_U matrix with the ssame size of the local_matrix
+    local_U = Eigen::Matrix<double,Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>(local_matrix.rows(), m);
+}
+
+void JacobiSolver::initializeMatrix(std::function<double(double, double)> func) {
+    int rows_per_process = n / size;
+
+    //this fills the f matrix with the computed values
+    if (rank == 0) {
+        Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> full_matrix(n, m);
+        int count_temp = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j){
+                // full_matrix(i, j) = func(static_cast<double>(i) / (n - 1), static_cast<double>(j) / (m - 1));
+                full_matrix(i, j) = count_temp++; //for testing purposes
+
+            }
+        }
+
+        // Share the full matrix row information with all processes
+        for (int r = 0; r < size; ++r) {
+            int start_row, end_row, num_rows, additional_row = 0;
+            int rest = n % size;
+
+            if (rest > r) {
+                start_row = r * (rows_per_process + 1) - 1;
+                additional_row++;
+            } else {
+                start_row = rest * (rows_per_process + 1) + (r - rest) * rows_per_process - 1;
+            }
+
+
+            if (r != 0 and r != size - 1) {
+                // start_row = r*rows_per_process - 1;
+                end_row = start_row + rows_per_process + 1 + additional_row;
+
+            }
+            else if (r == 0){
+                start_row = 0;
+                end_row = start_row + rows_per_process + additional_row;
+            }
+            else{ //at last rank we cannot have additional rows (otherwise rest is 0)
+                end_row = n - 1;
+            }
+            num_rows = end_row - start_row + 1;
+
+            if (r != 0) {
+                MPI_Send(&num_rows, 1, MPI_INT, r, 0, MPI_COMM_WORLD);
+                MPI_Send(full_matrix.data() + start_row * m, num_rows * m, MPI_DOUBLE, r, 0, MPI_COMM_WORLD);
+            } else {
+                local_matrix = full_matrix.block(start_row, 0, num_rows, m);
+            }
+        }
+
+    } else {
+        int num_rows;
+        MPI_Recv(&num_rows, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        local_matrix.resize(num_rows, m);
+        MPI_Recv(local_matrix.data(), num_rows * m, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+}
+
+
+void JacobiSolver::printLocalMatrixF() const {
+    if (rank == 0) {
+        // Rank 0 starts the printing process
+        std::cout << "Process " << rank << " local matrix F:" << std::endl;
+        std::cout << local_matrix << std::endl;
+        
+        if (size > 1) {
+            // Signal the next process to start printing
+            MPI_Send(nullptr, 0, MPI_BYTE, rank + 1, 0, MPI_COMM_WORLD);
+        }
+    } else {
+        // Other ranks wait for the signal to print
+        MPI_Recv(nullptr, 0, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        
+        std::cout << "Process " << rank << " local matrix F:" << std::endl;
+        std::cout << local_matrix << std::endl;
+
+        if (rank < size - 1) {
+            // Signal the next process to start printing
+            MPI_Send(nullptr, 0, MPI_BYTE, rank + 1, 0, MPI_COMM_WORLD);
+        }
+    }
+
+    // Ensure all processes complete their printing before proceeding
+    MPI_Barrier(MPI_COMM_WORLD);
+}
+
+
+void JacobiSolver::printLocalMatrixU() const {
+    if (rank == 0) {
+        // Rank 0 starts the printing process
+        std::cout << "Process " << rank << " local matrix U:" << std::endl;
+        std::cout << local_U << std::endl;
+        
+        if (size > 1) {
+            // Signal the next process to start printing
+            MPI_Send(nullptr, 0, MPI_BYTE, rank + 1, 0, MPI_COMM_WORLD);
+        }
+    } else {
+        // Other ranks wait for the signal to print
+        MPI_Recv(nullptr, 0, MPI_BYTE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        
+        std::cout << "Process " << rank << " local matrix U:" << std::endl;
+        std::cout << local_U << std::endl;
+
+        if (rank < size - 1) {
+            // Signal the next process to start printing
+            MPI_Send(nullptr, 0, MPI_BYTE, rank + 1, 0, MPI_COMM_WORLD);
+        }
+    }
+
+    // Ensure all processes complete their printing before proceeding
+    MPI_Barrier(MPI_COMM_WORLD);
+}
+
+void JacobiSolver::iterJacobi(){
+    //this method will generate the new matrix U at k+1 iteration
+    //the comunications between ranks will be done as follow:
+
+    //for each block of the local U compute the jacobi
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> U_new_buf(local_matrix.rows(), m);
+    // double h = 0.001;
+    double h = 0.1;
+
+    //compute the new matrix U 
+    for (int i = (rank > 0 ? 1 : 0); i < local_matrix.rows() - (rank < size - 1 ? 1 : 0); ++i) {
+        for (int j = 0; j < m; ++j) {
+            double sum = 0.0;
+            int count = 0;
+
+            // Check if the upper neighbor exists
+            if (i > 0 || rank > 0) {
+                sum += U_new_buf(i-1, j);
+                count++;
+            }
+            else if (i == 0) {
+                sum += 0.0;
+                count++;
+            }
+
+            // Check if the lower neighbor exists
+            if (i < local_matrix.rows() - 1 || rank < size - 1) {
+                sum += U_new_buf(i+1, j);
+                count++;
+            }
+
+            // Check if the left neighbor exists
+            if (j > 0) {
+                sum += U_new_buf(i, j-1);
+                count++;
+            }
+
+            // Check if the right neighbor exists
+            if (j < m - 1) {
+                sum += U_new_buf(i, j+1);
+                count++;
+            }
+
+            // U_new_buf(i, j) = (sum + h*h*local_matrix(i, j)) / count;
+            local_U(i, j) = 0.25 * (sum + h*h*local_matrix(i, j));
+            // U_new_buf(i, j) = 0.25 * (local_U(i-1, j) + local_U(i+1, j) + local_U(i, j-1) + local_U(i, j+1) + h*h*local_matrix(i, j));
+        }
+    }
+
+    // communicate the edge rows with the neighbors ranks
+    for(int r = 0; r < size; ++r){
+        int rows_per_process = n / size;
+        int start_row;
+        int num_rows = rows_per_process;
+        int rest = n % size;
+        //send the last row of the previous rank to the next rank
+        if (rank == r) {
+            if (rank != size - 1) {
+                MPI_Request send_request;
+                MPI_Isend(local_U.row(local_U.rows() - 2).data(), m, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &send_request);
+                
+                Eigen::VectorXd new_last_row(m);
+                MPI_Request recv_request;
+                MPI_Irecv(new_last_row.data(), m, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &recv_request);
+                
+                MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
+                local_U.row(local_U.rows() - 1) = new_last_row;
+                
+                MPI_Wait(&send_request, MPI_STATUS_IGNORE);
+            }
+        } else if (rank == r + 1) {
+            Eigen::VectorXd new_first_row(m);
+            MPI_Request recv_request;
+            MPI_Irecv(new_first_row.data(), m, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &recv_request);
+            
+            MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
+            local_U.row(0) = new_first_row;
+
+            MPI_Request send_request;
+            MPI_Isend(local_U.row(1).data(), m, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &send_request);
+            
+            MPI_Wait(&send_request, MPI_STATUS_IGNORE);
+        }
+    }
+
+    //now the local_U is ready for the next iteration
+
+}
