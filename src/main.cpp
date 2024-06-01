2276b230a5110ef26145602f9edf585214cb1a1d
NiccoGrillo <niccolo.grillo@mail.polimi.it>
2024-06-01 08:04:57 +0200

    first main used to test first methods

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <mpi.h>
+#include <iostream>
+#include "JacobiSolver.hpp"
+
+double myFunc(double x, double y) {
+    // return 8 * M_PI * M_PI * sin(2 * M_PI * x) * sin(2 * M_PI * y); //M_PI is pi greek letter
+    return x + y;
+}
+
+int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        std::cout << "Running with " << size << " processes" << std::endl;
+        std::cout << "Before iteration" << std::endl;
+    }
+
+    int n = 14;  // total number of rows
+    int m = 5;   // total number of columns
+
+
+    JacobiSolver solver(n, m, myFunc);
+
+
+
+    // solver.printLocalMatrixF();
+
+    // solver.printLocalMatrixU();
+
+    //test jacobi iteration
+    solver.iterJacobi();
+
+    // std::cout << "After iteration" << std::endl;
+    // solver.printLocalMatrixF();
+    solver.printLocalMatrixU();
+    
+
+    MPI_Finalize();
+    return 0;
+}
