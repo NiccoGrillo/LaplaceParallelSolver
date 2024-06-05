ab63ce5249cb434e6873a325cbe5306321246030
NiccoGrillo <niccolo.grillo@mail.polimi.it>
2024-06-05 11:51:10 +0200

    new tests with robin and multithreading control

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,58 @@
-#include <mpi.h>
-#include <iostream>
-#include <cmath>
 #include "JacobiSolver.hpp"
-// #include "writeVTK.hpp"
+#include "DirichletBoundaryCondition.hpp"
+#include "RobinBoundaryCondition.hpp"
 
-double myFunc(double x, double y) {
-    return 8 * M_PI * M_PI * sin(2 * M_PI * x) * sin(2 * M_PI * y);
-}
-
-double exactSolution(double x, double y) {
-    return sin(2 * M_PI * x) * sin(2 * M_PI * y);
-}
-
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+
+
+    int num = 30;
+    int max_iters = 100000;
+    double tol = 1e-12;
+    bool use_multithreading = true;
     if (rank == 0) {
-        std::cout << "Running with " << size << " processes" << std::endl;
+        std::cout << "Number of grid points: " << num << std::endl;
+        std::cout << "Maximum number of iterations: " << max_iters << std::endl;
+        std::cout << "Tolerance: " << tol << std::endl;
+        std::cout << "Using multithreading: " << (use_multithreading ? "true" : "false") << "\n\n" << std::endl;
     }
 
-    int n = 200;  // total number of rows
-    int max_iters = 200000;
-    double tol = 1e-13;
+    auto func = [](double x, double y) { return 8 * M_PI * M_PI * sin(2 * M_PI * x) * sin(2 * M_PI * y); };
+    auto exact_sol = [](double x, double y) { return sin(2 * M_PI * x) * sin(2 * M_PI * y); };
 
-    JacobiSolver solver(n, max_iters, tol, myFunc, exactSolution);
-
-    // solver.iterJacobi();
-    solver.solve();
-
-    double l2_error = solver.computeL2Error();
+    // For Dirichlet boundary condition
+    if(rank ==0)
+        std::cout << "Dirichlet boundary condition" << std::endl;
+    DirichletBoundaryCondition dirichlet_bc;
+    JacobiSolver<DirichletBoundaryCondition> solverDirichlet(num, max_iters, tol, func, exact_sol, dirichlet_bc, use_multithreading);
+    solverDirichlet.solve();
+    solverDirichlet.saveSolution("solution_dirichlet.vtk", true, false);
+    double l2_error_dir = solverDirichlet.computeL2Error();
 
     if (rank == 0) {
-        std::cout << "L2 Error: " << l2_error << std::endl;
+        std::cout << "L2 Error: " << l2_error_dir << std::endl;
     }
-         
-    solver.saveSolution("solution.vtk", true, false); // save also the exact solution and do not print the solution
+
+    // For Robin boundary condition
+    if (rank == 0)
+        std::cout << "\n\nRobin boundary condition" << std::endl;
+    double alpha = 1.0; // example value
+    double beta = 1.0;  // example value
+    auto g_func = [](double x, double y) { return cos(2 * M_PI * x) + sin(2 * M_PI * y); }; // Example boundary function
+    RobinBoundaryCondition robin_bc(alpha, beta, g_func);
+    JacobiSolver<RobinBoundaryCondition> solverRobin(num, max_iters, tol, func, exact_sol, robin_bc, use_multithreading);
+    solverRobin.solve();
+    solverRobin.saveSolution("solution_robin.vtk", true, false);
+    double l2_error_robin = solverRobin.computeL2Error();
+
+    if (rank == 0) {
+        std::cout << "L2 Error: " << l2_error_robin << std::endl;
+    }    
 
     MPI_Finalize();
     return 0;
